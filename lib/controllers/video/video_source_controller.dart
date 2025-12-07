a741d8ee474a57d42f6d12629b0519f47770785d
LiggMax <wenzhouli06@gmail.com>
2025-12-07 19:02:19 +0800

    backup(video_source_controller.dart)

--- lib/controllers/video/video_source_controller.dart
+++ lib/controllers/video/video_source_controller.dart
@@ -1,5 +1,7 @@
 import 'package:anime_flow/data/crawler/html_request.dart';
+import 'package:anime_flow/models/item/crawler_config_item.dart';
 import 'package:anime_flow/models/item/video/episode_resources_item.dart';
+import 'package:anime_flow/models/item/video/resources_item.dart';
 import 'package:anime_flow/models/item/video/search_resources_item.dart';
 import 'package:anime_flow/utils/crawl_config.dart';
 import 'package:get/get.dart';
@@ -8,25 +10,19 @@ import 'package:get/get.dart';
 class VideoSourceController extends GetxController {
   final RxString videoRul = ''.obs;
 
-
   void setVideoUrl(String url) {
     videoRul.value = url;
   }
 
-
-  Future<List<EpisodeResourcesItem>> getVideoResources(String keyword) async {
-    final configs = await CrawlConfig.loadAllCrawlConfigs();
-    for (var config in configs) {
-
-    }
+  //获取剧集资源
+  Future<List<EpisodeResourcesItem>> getEpisodeResources(
+      String keyword, CrawlConfigItem config) async {
     List<SearchResourcesItem> searchList =
-        await WebRequest.getSearchSubjectListService(
-      keyword,
-    );
+        await WebRequest.getSearchSubjectListService(keyword, config);
     List<EpisodeResourcesItem> allEpisodesList = [];
     for (var search in searchList) {
       var crawlerEpisodeResources =
-          await WebRequest.getResourcesListService(search.link);
+          await WebRequest.getResourcesListService(search.link, config);
 
       // 转换 CrawlerEpisodeResourcesItem 到 EpisodeResourcesItem
       for (var crawlerResource in crawlerEpisodeResources) {

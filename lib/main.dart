261a40e6d3c390d4180ffe3f9dd3469c51c29ab0
LiggMax <wenzhouli06@gmail.com>
2025-12-18 03:57:13 +0800

    fix(theme): 初始化主题控制器
    - 在应用启动时调用themeController.initTheme()方法，解决主题未持久化问题

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    themeController.initTheme();
     return GetBuilder<ThemeController>(
       builder: (controller) {
         return GetMaterialApp(

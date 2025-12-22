a70ad7d36aba88ff2658e952fbf541dfe0ede8fe
LiggMax <wenzhouli06@gmail.com>
2025-12-22 08:17:46 +0800

    fix(api): 修复动漫流API地址拼写错误
    - 修正了AnimeFlowApi类中的API地址常量名称拼写错误，将ainmeFlowApi更正为animeFlowApi

--- lib/http/api/anime_flow_api.dart
+++ lib/http/api/anime_flow_api.dart
@@ -1,5 +1,7 @@
 class AnimeFlowApi {
-  static const String ainmeFlowApi = 'http://192.168.3.62:1024';
+  static const String animeFlowApi = 'http://192.168.3.62:1024';
 
   static const String token = '/oauth/token';
+
+  static const String session = '/oauth/session';
 }
--- lib/http/requests/oauth_request.dart
+++ lib/http/requests/oauth_request.dart
@@ -1,15 +1,22 @@
 import 'package:anime_flow/http/api/anime_flow_api.dart';
 import 'package:anime_flow/http/dio/bgm_dio_request.dart';
+import 'package:anime_flow/http/dio/dio_request.dart';
 import 'package:anime_flow/models/item/token_item.dart';
-import 'package:logger/logger.dart';
 
 class OAuthRequest {
-  static const String _animeFlowApi = AnimeFlowApi.ainmeFlowApi;
+  static const String _animeFlowApi = AnimeFlowApi.animeFlowApi;
 
   static Future<TokenItem> getTokenService({required String code}) async {
     final response = await bgmDioRequest.post(
         _animeFlowApi + AnimeFlowApi.token,
         queryParameters: {'code': code});
     return TokenItem.fromJson(response.data['data']);
+  }
+
+  //获取session
+  static Future<Map<String, dynamic>> getSessionService() async {
+    return await dioRequest
+        .get(_animeFlowApi + AnimeFlowApi.session)
+        .then((value) => value.data);
   }
 }

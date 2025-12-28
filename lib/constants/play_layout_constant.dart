7464a47396f557be37a4008587bd20dcba07991e
LiggMax <wenzhouli06@gmail.com>
2025-12-28 19:34:51 +0800

    refactor(constants): 调整页面宽度常量配置

--- lib/constants/play_layout_constant.dart
+++ lib/constants/play_layout_constant.dart
@@ -3,6 +3,6 @@ class PlayLayoutConstant {
   ///播放页面内容宽度
   static const double playContentWidth = 350.0;
 
-  ///详情页最大宽度
-  static const double infoMaxWidth = 1200.0;
+  ///页面最大宽度
+  static const double maxWidth = 1800.0;
 }

86e9a423dfe7b050ebc9e52356a95e99ef5284ea
LiggMax <wenzhouli06@gmail.com>
2025-12-28 19:38:37 +0800

    refactor(calendar): 使用布局常量替代硬编码宽度值

--- lib/pages/calendar/index.dart
+++ lib/pages/calendar/index.dart
@@ -1,3 +1,4 @@
+import 'package:anime_flow/constants/play_layout_constant.dart';
 import 'package:anime_flow/models/item/bangumi/calendar_item.dart';
 import 'package:anime_flow/models/item/subject_basic_data_item.dart';
 import 'package:anime_flow/routes/index.dart';
@@ -124,7 +125,7 @@ class _CalendarPageState extends State<CalendarPage>
 
     return Center(
       child: ConstrainedBox(
-        constraints: const BoxConstraints(maxWidth: 1800),
+        constraints: const BoxConstraints(maxWidth: PlayLayoutConstant.maxWidth),
         child: CustomScrollView(
           slivers: [
             // 统计信息

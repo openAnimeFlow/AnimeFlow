05d5de27892696814ee67b50364febf7b84bf1e7
LiggMax <wenzhouli06@gmail.com>
2026-01-14 10:54:34 +0800

    feat(subject): 添加标签数据管理和更新功能
    - 引入 Tags 模型并导入 SubjectsInfoItem
    - 添加 tags 响应式列表属性用于存储标签数据
    - 修改 setSubject 方法以支持标签参数传递
    - 在设置主题信息时同步更新标签数据

--- lib/controllers/subject/subject_state_controller.dart
+++ lib/controllers/subject/subject_state_controller.dart
@@ -1,14 +1,18 @@
 import 'package:get/get.dart';
+import 'package:anime_flow/models/item/bangumi/subjects_info_item.dart';
 
 class SubjectStateController extends GetxController {
   final RxString subjectName = ''.obs;
   final RxInt subjectId = 0.obs;
+  final RxList<Tags> tags = <Tags>[].obs;
 
   String get name => subjectName.value;
+
   int get id => subjectId.value;
 
-  void setSubject(String name, int subjectId) {
+  void setSubject(String name, int subjectId, List<Tags> tags) {
     subjectName.value = name;
+    this.tags.value = tags;
     this.subjectId.value = subjectId;
   }
 }

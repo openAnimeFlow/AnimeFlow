2c8ea8dc8a4cd3e762f2aaab2060b5c94011aa2b
LiggMax <wenzhouli06@gmail.com>
2026-01-15 20:28:41 +0800

    refactor(bangumi): 重构剧集数据模型
    - 将Data类重命名为EpisodeData并优化属性定义
    - 更新Collection类为EpisodeCollection并改进结构
    - 简化EpisodesItem类的JSON序列化和反序列化逻辑
    - 使用更简洁的构造函数语法初始化对象
    - 添加空安全处理以提高代码健壮性
    - 移除不必要的getter方法直接使用公共字段
    - 优化fromJson和toJson方法的实现逻辑

--- lib/models/item/bangumi/episodes_item.dart
+++ lib/models/item/bangumi/episodes_item.dart
@@ -1,203 +1,107 @@
 class EpisodesItem {
+  final List<EpisodeData> data;
+  final int total;
+
   EpisodesItem({
-    required List<Data> data,
-    required int total,
-  }) {
-    _data = data;
-    _total = total;
-  }
+    required this.data,
+    required this.total,
+  });
 
-  EpisodesItem.fromJson(dynamic json) {
-    if (json['data'] != null) {
-      _data = [];
-      json['data'].forEach((v) {
-        _data.add(Data.fromJson(v));
-      });
-    }
-    _total = json['total'];
-  }
-
-  late List<Data> _data;
-  late int _total;
-
-  EpisodesItem copyWith({
-    List<Data>? data,
-    int? total,
-  }) =>
-      EpisodesItem(
-        data: data ?? _data,
-        total: total ?? _total,
-      );
-
-  List<Data> get data => _data;
-  int get total => _total;
+  EpisodesItem.fromJson(Map<String, dynamic> json)
+      : data = (json['data'] as List?)
+                ?.map((e) => EpisodeData.fromJson(e))
+                .toList() ??
+            [],
+        total = json['total'] ?? 0;
 
   Map<String, dynamic> toJson() {
-    final map = <String, dynamic>{};
-    map['data'] = _data.map((v) => v.toJson()).toList();
-      map['total'] = _total;
-    return map;
+    return {
+      'data': data.map((e) => e.toJson()).toList(),
+      'total': total,
+    };
   }
 }
 
-class Data {
-  Data({
-    required int id,
-    required int subjectID,
-    required num sort,
-    required int type,
-    required int disc,
-    required String name,
-    required String nameCN,
-    required String duration,
-    required String airdate,
-    required int comment,
-    required String desc,
-    Collection? collection,
-  }) {
-    _id = id;
-    _subjectID = subjectID;
-    _sort = sort;
-    _type = type;
-    _disc = disc;
-    _name = name;
-    _nameCN = nameCN;
-    _duration = duration;
-    _airdate = airdate;
-    _comment = comment;
-    _desc = desc;
-    _collection = collection;
-  }
+class EpisodeData {
+  final int id;
+  final int subjectID;
+  final num sort;
+  final int type;
+  final int disc;
+  final String name;
+  final String? nameCN;
+  final String duration;
+  final String airdate;
+  final int comment;
+  final String desc;
+  final EpisodeCollection? collection;
 
-  Data.fromJson(dynamic json) {
-    _id = json['id'];
-    _subjectID = json['subjectID'];
-    _sort = json['sort'];
-    _type = json['type'];
-    _disc = json['disc'];
-    _name = json['name'];
-    _nameCN = json['nameCN'];
-    _duration = json['duration'];
-    _airdate = json['airdate'];
-    _comment = json['comment'];
-    _desc = json['desc'];
-    _collection =
-        json['collection'] != null ? Collection.fromJson(json['collection']) : null;
-  }
+  EpisodeData({
+    required this.id,
+    required this.subjectID,
+    required this.sort,
+    required this.type,
+    required this.disc,
+    required this.name,
+    this.nameCN,
+    required this.duration,
+    required this.airdate,
+    required this.comment,
+    required this.desc,
+    this.collection,
+  });
 
-  late int _id;
-  late int _subjectID;
-  late num _sort;
-  late int _type;
-  late int _disc;
-  late String _name;
-  late String _nameCN;
-  late String _duration;
-  late String _airdate;
-  late int _comment;
-  late String _desc;
-  Collection? _collection;
-
-  Data copyWith({
-    int? id,
-    int? subjectID,
-    num? sort,
-    int? type,
-    int? disc,
-    String? name,
-    String? nameCN,
-    String? duration,
-    String? airdate,
-    int? comment,
-    String? desc,
-    Collection? collection,
-  }) =>
-      Data(
-        id: id ?? _id,
-        subjectID: subjectID ?? _subjectID,
-        sort: sort ?? _sort,
-        type: type ?? _type,
-        disc: disc ?? _disc,
-        name: name ?? _name,
-        nameCN: nameCN ?? _nameCN,
-        duration: duration ?? _duration,
-        airdate: airdate ?? _airdate,
-        comment: comment ?? _comment,
-        desc: desc ?? _desc,
-        collection: collection ?? _collection,
-      );
-
-  int get id => _id;
-  int get subjectID => _subjectID;
-  num get sort => _sort;
-  int get type => _type;
-  int get disc => _disc;
-  String get name => _name;
-  String get nameCN => _nameCN;
-  String get duration => _duration;
-  String get airdate => _airdate;
-  int get comment => _comment;
-  String get desc => _desc;
-  Collection? get collection => _collection;
+  EpisodeData.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        subjectID = json['subjectID'],
+        sort = json['sort'],
+        type = json['type'],
+        disc = json['disc'],
+        name = json['name'] ?? '',
+        nameCN = json['nameCN'],
+        duration = json['duration'] ?? '',
+        airdate = json['airdate'] ?? '',
+        comment = json['comment'] ?? 0,
+        desc = json['desc'] ?? '',
+        collection = json['collection'] != null
+            ? EpisodeCollection.fromJson(json['collection'])
+            : null;
 
   Map<String, dynamic> toJson() {
-    final map = <String, dynamic>{};
-    map['id'] = _id;
-    map['subjectID'] = _subjectID;
-    map['sort'] = _sort;
-    map['type'] = _type;
-    map['disc'] = _disc;
-    map['name'] = _name;
-    map['nameCN'] = _nameCN;
-    map['duration'] = _duration;
-    map['airdate'] = _airdate;
-    map['comment'] = _comment;
-    map['desc'] = _desc;
-    if (_collection != null) {
-      map['collection'] = _collection?.toJson();
-    }
-    return map;
+    return {
+      'id': id,
+      'subjectID': subjectID,
+      'sort': sort,
+      'type': type,
+      'disc': disc,
+      'name': name,
+      'nameCN': nameCN,
+      'duration': duration,
+      'airdate': airdate,
+      'comment': comment,
+      'desc': desc,
+      if (collection != null) 'collection': collection!.toJson(),
+    };
   }
 }
 
-/// status : 2
-/// updatedAt : 1761630252
+class EpisodeCollection {
+  final int status;
+  final int? updatedAt;
 
-class Collection {
-  Collection({
-    required int status,
-    int? updatedAt,
-  }) {
-    _status = status;
-    _updatedAt = updatedAt;
-  }
+  EpisodeCollection({
+    required this.status,
+    this.updatedAt,
+  });
 
-  Collection.fromJson(dynamic json) {
-    _status = json['status'];
-    _updatedAt = json['updatedAt'];
-  }
-
-  late int _status;
-  int? _updatedAt;
-
-  Collection copyWith({
-    int? status,
-    int? updatedAt,
-  }) =>
-      Collection(
-        status: status ?? _status,
-        updatedAt: updatedAt ?? _updatedAt,
-      );
-
-  int get status => _status;
-  int? get updatedAt => _updatedAt;
+  EpisodeCollection.fromJson(Map<String, dynamic> json)
+      : status = json['status'],
+        updatedAt = json['updatedAt'];
 
   Map<String, dynamic> toJson() {
-    final map = <String, dynamic>{};
-    map['status'] = _status;
-    if (_updatedAt != null) {
-      map['updatedAt'] = _updatedAt;
-    }
-    return map;
+    return {
+      'status': status,
+      if (updatedAt != null) 'updatedAt': updatedAt,
+    };
   }
 }

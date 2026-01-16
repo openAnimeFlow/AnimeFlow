142473861be675a65a7b7dc5d3db25232cf013e8
LiggMax <wenzhouli06@gmail.com>
2026-01-16 13:29:29 +0800

    refactor(main): 移除未使用的 WebView 相关依赖

--- lib/pages/main/index.dart
+++ lib/pages/main/index.dart
@@ -9,7 +9,6 @@ import 'package:anime_flow/utils/utils.dart';
 import 'package:anime_flow/widget/animation_network_image/animation_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:anime_flow/pages/recommend/index.dart';
-import 'package:flutter_inappwebview_platform_interface/flutter_inappwebview_platform_interface.dart';
 import 'package:get/get.dart';
 
 class MainPage extends StatefulWidget {

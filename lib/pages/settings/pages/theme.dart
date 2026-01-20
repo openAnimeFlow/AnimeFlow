844c14ca7fcfac75fd0da1c248fb06e535cdb07a
LiggMax <wenzhouli06@gmail.com>
2026-01-20 19:38:55 +0800

    fix(theme): 修复主题设置页面布局和边距问题
    - 添加了左侧媒体查询边距以正确处理设备安全区域
    - 调整了ListView的内边距逻辑，适配不同设备的边距需求

--- lib/pages/settings/pages/theme.dart
+++ lib/pages/settings/pages/theme.dart
@@ -28,7 +28,7 @@ class _ThemePageState extends State<ThemePage> {
   @override
   Widget build(BuildContext context) {
     final themeMode = themeController.themeMode;
-
+    final leftMediaQueryPadding = MediaQuery.of(context).padding.left;
     return GetBuilder<ThemeController>(
         builder: (themeCtrl) => Scaffold(
               appBar: AppBar(
@@ -38,10 +38,13 @@ class _ThemePageState extends State<ThemePage> {
               ),
               body: Center(
                 child: ConstrainedBox(
-                  constraints: const BoxConstraints(
-                      maxWidth: 1200),
+                  constraints: const BoxConstraints(maxWidth: 1200),
                   child: ListView(
-                    padding: const EdgeInsets.all(16),
+                    padding: EdgeInsets.only(
+                        left: leftMediaQueryPadding == 0
+                            ? 16
+                            : leftMediaQueryPadding,
+                        right: 16),
                     children: [
                       // 主题模式选择
                       const Text(
@@ -54,81 +57,78 @@ class _ThemePageState extends State<ThemePage> {
                       const SizedBox(height: 12),
                       GetBuilder<ThemeController>(
                         builder: (controller) {
-                          return  IntrinsicWidth(
-                              child: glassPanel(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 8, vertical: 10),
-                                child: Row(
-                                  children: [
-                                    GestureDetector(
-                                      onTap: () {
-                                        setState(() {
-                                          controller
-                                              .setThemeMode(ThemeMode.dark);
-                                        });
-                                      },
+                          return IntrinsicWidth(
+                            child: glassPanel(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 8, vertical: 10),
+                              child: Row(
+                                children: [
+                                  GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        controller.setThemeMode(ThemeMode.dark);
+                                      });
+                                    },
+                                    child: ThemePreviewCard(
+                                      bg: const Color(0xFF020617),
+                                      primary: const Color(0xFF3B82F6),
+                                      titleColor: Colors.white,
+                                      subtitleColor: const Color(0xFF6B7280),
+                                      icon: Icons.nightlight_round,
+                                      title: "深色模式",
+                                      subtitle: "深色护眼",
+                                      selected: themeMode == ThemeMode.dark,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 5),
+                                  GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        controller
+                                            .setThemeMode(ThemeMode.light);
+                                      });
+                                    },
+                                    child: ThemePreviewCard(
+                                      bg: const Color(0xFFF8FAFC),
+                                      primary: const Color(0xFFFACC15),
+                                      titleColor: Colors.black,
+                                      subtitleColor: Colors.black54,
+                                      icon: Icons.wb_sunny,
+                                      title: "浅色模式",
+                                      subtitle: "明亮清爽",
+                                      selected: themeMode == ThemeMode.light,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 5),
+                                  GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        controller
+                                            .setThemeMode(ThemeMode.system);
+                                      });
+                                    },
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(18),
+                                      ),
                                       child: ThemePreviewCard(
                                         bg: const Color(0xFF020617),
-                                        primary: const Color(0xFF3B82F6),
+                                        primary: Theme.of(context)
+                                            .colorScheme
+                                            .primary,
                                         titleColor: Colors.white,
-                                        subtitleColor: const Color(0xFF6B7280),
-                                        icon: Icons.nightlight_round,
-                                        title: "深色模式",
-                                        subtitle: "深色护眼",
-                                        selected: themeMode == ThemeMode.dark,
+                                        subtitleColor: Colors.white60,
+                                        icon: Icons.settings,
+                                        title: "跟随系统",
+                                        subtitle: "自动适配",
+                                        overlay: const DiagonalOverlay(),
+                                        selected: themeMode == ThemeMode.system,
                                       ),
                                     ),
-                                    const SizedBox(width: 5),
-                                    GestureDetector(
-                                      onTap: () {
-                                        setState(() {
-                                          controller
-                                              .setThemeMode(ThemeMode.light);
-                                        });
-                                      },
-                                      child: ThemePreviewCard(
-                                        bg: const Color(0xFFF8FAFC),
-                                        primary: const Color(0xFFFACC15),
-                                        titleColor: Colors.black,
-                                        subtitleColor: Colors.black54,
-                                        icon: Icons.wb_sunny,
-                                        title: "浅色模式",
-                                        subtitle: "明亮清爽",
-                                        selected: themeMode == ThemeMode.light,
-                                      ),
-                                    ),
-                                    const SizedBox(width: 5),
-                                    GestureDetector(
-                                      onTap: () {
-                                        setState(() {
-                                          controller
-                                              .setThemeMode(ThemeMode.system);
-                                        });
-                                      },
-                                      child: Container(
-                                        decoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(18),
-                                        ),
-                                        child: ThemePreviewCard(
-                                          bg: const Color(0xFF020617),
-                                          primary: Theme.of(context)
-                                              .colorScheme
-                                              .primary,
-                                          titleColor: Colors.white,
-                                          subtitleColor: Colors.white60,
-                                          icon: Icons.settings,
-                                          title: "跟随系统",
-                                          subtitle: "自动适配",
-                                          overlay: const DiagonalOverlay(),
-                                          selected:
-                                              themeMode == ThemeMode.system,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
+                                  ),
+                                ],
                               ),
+                            ),
                           );
                         },
                       ),
@@ -150,7 +150,8 @@ class _ThemePageState extends State<ThemePage> {
                             physics: const NeverScrollableScrollPhysics(),
                             gridDelegate:
                                 SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: LayoutUtil.getCrossAxisCount(context),
+                              crossAxisCount:
+                                  LayoutUtil.getCrossAxisCount(context),
                               crossAxisSpacing: 5, // 横向间距
                               mainAxisSpacing: 10, // 纵向间距
                               childAspectRatio: 0.7, // 宽高比

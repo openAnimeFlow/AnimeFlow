70a9cb07c97e4c1f9b635fb5b2f329bf187b5c4c
LiggMax <wenzhouli06@gmail.com>
2026-04-18 04:46:36 +0800

    Revert "refactor(network_speed): 迁移到 Riverpod 注解生成器"
    This reverts commit 966c3c5a

--- lib/features/network_speed/presentation/network_speed_provider.dart
+++ lib/features/network_speed/presentation/network_speed_provider.dart
@@ -1,25 +1,24 @@
 import 'dart:async';
-
 import 'package:anime_flow/features/network_speed/data/network_speed_service.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-part 'network_speed_provider.g.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// 用于依赖注入：每次调用都创建一个独立实例。
-@riverpod
-NetworkSpeedService Function() networkSpeedServiceFactory(Ref ref) {
-  return () => NetworkSpeedService();
-}
+final networkSpeedServiceFactoryProvider =
+    Provider<NetworkSpeedService Function()>(
+  (ref) => () => NetworkSpeedService(),
+);
 
 /// Riverpod 托管 start/stop 生命周期。
 /// - 首次 watch 时自动 start
 /// - autoDispose 时自动 stop
-@riverpod
-Stream<NetworkSpeed> networkSpeedStream(Ref ref, int intervalMs) async* {
-  final service = ref.read(networkSpeedServiceFactoryProvider)();
-  final stream = service.start(interval: intervalMs);
-  ref.onDispose(() {
-    unawaited(service.stop());
-  });
-  yield* stream;
-}
+final networkSpeedStreamProvider =
+    StreamProvider.autoDispose.family<NetworkSpeed, int>(
+  (ref, intervalMs) {
+    final service = ref.read(networkSpeedServiceFactoryProvider)();
+    final stream = service.start(interval: intervalMs);
+    ref.onDispose(() {
+      unawaited(service.stop());
+    });
+    return stream;
+  },
+);

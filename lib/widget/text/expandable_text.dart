4e6cd5a75179c1b3d79106eb253759a0320aad76
LiggMax <wenzhouli06@gmail.com>
2026-05-15 16:52:32 +0800

    refactor(widget): 使用Column的spacing属性替代SizedBox实现间距

--- lib/widget/text/expandable_text.dart
+++ lib/widget/text/expandable_text.dart
@@ -30,124 +30,127 @@ class _ExpandableTextState extends State<ExpandableText> {
   Widget build(BuildContext context) {
     return SizedBox(
         width: double.infinity,
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Text(
-            widget.title,
-            textAlign: TextAlign.start,
-            style: TextStyle(
-                fontSize: widget.fontSizeTitle,
-                fontWeight: widget.fontWeightTitle),
-          ),
-          const SizedBox(height: 5),
-          LayoutBuilder(
-            builder: (context, constraints) {
-              final textPainter = TextPainter(
-                text: TextSpan(
-                    text: widget.text,
-                    style: TextStyle(fontWeight: widget.fontWeight)),
-                maxLines: widget.maxLines,
-                textDirection: TextDirection.ltr,
-              );
-              textPainter.layout(maxWidth: constraints.maxWidth);
+        child: Column(
+            spacing: 5,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.title,
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                    fontSize: widget.fontSizeTitle,
+                    fontWeight: widget.fontWeightTitle),
+              ),
+              LayoutBuilder(
+                builder: (context, constraints) {
+                  final textPainter = TextPainter(
+                    text: TextSpan(
+                        text: widget.text,
+                        style: TextStyle(fontWeight: widget.fontWeight)),
+                    maxLines: widget.maxLines,
+                    textDirection: TextDirection.ltr,
+                  );
+                  textPainter.layout(maxWidth: constraints.maxWidth);
 
-              final showExpandButton = textPainter.didExceedMaxLines;
+                  final showExpandButton = textPainter.didExceedMaxLines;
 
-              if (_isExpanded) {
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: double.infinity,
-                      child: InkWell(
-                        onTap: showExpandButton
-                            ? () {
+                  if (_isExpanded) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: double.infinity,
+                          child: InkWell(
+                            onTap: showExpandButton
+                                ? () {
+                                    setState(() {
+                                      _isExpanded = false;
+                                    });
+                                  }
+                                : null,
+                            child: Text(
+                              widget.text,
+                              style: TextStyle(fontWeight: widget.fontWeight),
+                            ),
+                          ),
+                        ),
+                        if (showExpandButton)
+                          Align(
+                            alignment: Alignment.center,
+                            child: IconButton(
+                              onPressed: () {
                                 setState(() {
                                   _isExpanded = false;
                                 });
-                              }
-                            : null,
-                        child: Text(
-                          widget.text,
-                          style: TextStyle(fontWeight: widget.fontWeight),
-                        ),
-                      ),
-                    ),
-                    if (showExpandButton)
-                      Align(
-                        alignment: Alignment.center,
-                        child: IconButton(
-                          onPressed: () {
+                              },
+                              icon: const Icon(Icons.expand_less_rounded,
+                                  size: 30),
+                            ),
+                          ),
+                      ],
+                    );
+                  }
+
+                  return Stack(
+                    children: [
+                      SizedBox(
+                        width: double.infinity,
+                        child: InkWell(
+                          onTap: () {
                             setState(() {
-                              _isExpanded = false;
+                              _isExpanded = true;
                             });
                           },
-                          icon: const Icon(Icons.expand_less_rounded, size: 30),
-                        ),
-                      ),
-                  ],
-                );
-              }
-
-              return Stack(
-                children: [
-                  SizedBox(
-                    width: double.infinity,
-                    child: InkWell(
-                      onTap: () {
-                        setState(() {
-                          _isExpanded = true;
-                        });
-                      },
-                      child: Text(
-                        widget.text,
-                        style: TextStyle(fontWeight: widget.fontWeight),
-                        maxLines: widget.maxLines,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                  ),
-                  if (showExpandButton)
-                    Positioned(
-                      bottom: 0,
-                      left: 0,
-                      right: 0,
-                      child: Container(
-                        height: 50,
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            colors: [
-                              Theme.of(context)
-                                  .scaffoldBackgroundColor
-                                  .withValues(alpha: 0),
-                              Theme.of(context)
-                                  .scaffoldBackgroundColor
-                                  .withValues(alpha: 0.8),
-                              Theme.of(context).scaffoldBackgroundColor,
-                            ],
-                            stops: const [0.0, 0.7, 1.0],
-                          ),
-                        ),
-                        child: Align(
-                          alignment: Alignment.bottomCenter,
-                          child: IconButton(
-                            padding: EdgeInsets.zero,
-                            onPressed: () {
-                              setState(() {
-                                _isExpanded = true;
-                              });
-                            },
-                            icon:
-                                const Icon(Icons.expand_more_rounded, size: 30),
+                          child: Text(
+                            widget.text,
+                            style: TextStyle(fontWeight: widget.fontWeight),
+                            maxLines: widget.maxLines,
+                            overflow: TextOverflow.ellipsis,
                           ),
                         ),
                       ),
-                    ),
-                ],
-              );
-            },
-          )
-        ]));
+                      if (showExpandButton)
+                        Positioned(
+                          bottom: 0,
+                          left: 0,
+                          right: 0,
+                          child: Container(
+                            height: 50,
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                                colors: [
+                                  Theme.of(context)
+                                      .scaffoldBackgroundColor
+                                      .withValues(alpha: 0),
+                                  Theme.of(context)
+                                      .scaffoldBackgroundColor
+                                      .withValues(alpha: 0.8),
+                                  Theme.of(context).scaffoldBackgroundColor,
+                                ],
+                                stops: const [0.0, 0.7, 1.0],
+                              ),
+                            ),
+                            child: Align(
+                              alignment: Alignment.bottomCenter,
+                              child: IconButton(
+                                padding: EdgeInsets.zero,
+                                onPressed: () {
+                                  setState(() {
+                                    _isExpanded = true;
+                                  });
+                                },
+                                icon: const Icon(Icons.expand_more_rounded,
+                                    size: 30),
+                              ),
+                            ),
+                          ),
+                        ),
+                    ],
+                  );
+                },
+              )
+            ]));
   }
 }

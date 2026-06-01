b3a650a1f7c4abb898e70984221dd5ab73c5b4cf
LiggMax <wenzhouli06@gmail.com>
2026-06-02 01:29:39 +0800

    feat(utils): 添加字符串相似度计算功能
    - 实现了 calculateSimilarity 方法用于计算两个字符串的相似度
    - 实现了 levenshteinDistance 方法用于计算编辑距离

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,13 +1,14 @@
 import 'dart:io';
 import 'dart:math';
+
 import 'package:anime_flow/constants/constants.dart';
 import 'package:anime_flow/http/api_path.dart';
+import 'package:anime_flow/utils/logger.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_inappwebview_platform_interface/flutter_inappwebview_platform_interface.dart';
 import 'package:path/path.dart' as path;
-import 'package:anime_flow/utils/logger.dart';
 
 class Utils {
   static LiggLogger logger = LiggLogger();
@@ -192,5 +193,36 @@ class Utils {
     }
 
     return url;
+  }
+
+  static double calculateSimilarity(String s1, String s2) {
+    int maxLength = max(s1.length, s2.length);
+    if (maxLength == 0) return 1.0;
+    if (s1 == s2) return 1.0;
+    return (1.0 - levenshteinDistance(s1, s2) / maxLength);
+  }
+
+  static int levenshteinDistance(String s1, String s2) {
+    if (s1 == s2) return 0;
+    if (s1.isEmpty) return s2.length;
+    if (s2.isEmpty) return s1.length;
+
+    List<int> v0 = List<int>.generate(s2.length + 1, (i) => i);
+    List<int> v1 = List<int>.filled(s2.length + 1, 0);
+
+    for (int i = 0; i < s1.length; i++) {
+      v1[0] = i + 1;
+
+      for (int j = 0; j < s2.length; j++) {
+        int cost = (s1[i] == s2[j]) ? 0 : 1;
+        v1[j + 1] = min(v1[j] + 1, min(v0[j + 1] + 1, v0[j] + cost));
+      }
+
+      for (int j = 0; j < v0.length; j++) {
+        v0[j] = v1[j];
+      }
+    }
+
+    return v1[s2.length];
   }
 }

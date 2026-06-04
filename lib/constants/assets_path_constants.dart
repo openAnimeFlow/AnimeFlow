0c4e56e712f01cca2d9929e6340753196ecd57b5
LiggMax <wenzhouli06@gmail.com>
2026-06-04 10:52:14 +0800

    update(image): 更新logo图片

--- lib/constants/assets_path_constants.dart
+++ lib/constants/assets_path_constants.dart
@@ -1,7 +1,7 @@
 class AssetsPathConstants {
 
   /// 用于Logo
-  static const String logo = 'assets/logo/startLogo.webp';
+  static const String logo = 'assets/logo/logo.webp';
 
   /// 播放动画
   static const String playJsonIng = 'assets/animation/playing.json';

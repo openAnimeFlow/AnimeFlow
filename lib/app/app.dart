7857380454b91a17e902148553bf2da8dead0d4b
LiggMax <wenzhouli06@gmail.com>
2026-08-01 19:03:09 +0800

    refactor: reorganize app version file structure

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -5,7 +5,7 @@ import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:anime_flow/shared/widgets/windows_title_bar.dart';
-import 'package:anime_flow/app_version.dart';
+import 'package:anime_flow/app/app_version.dart';
 import 'package:anime_flow/core/constants/assets_path_constants.dart';
 
 class MyApp extends StatelessWidget {
--- lib/app/app_version.dart
+++ lib/app/app_version.dart
@@ -5,8 +5,8 @@ import 'package:anime_flow/features/app_update/presentation/widgets/version_upda
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'core/apply_update/app_info_provider.dart';
-import 'core/apply_update/app_info_state.dart';
+import '../core/apply_update/app_info_provider.dart';
+import '../core/apply_update/app_info_state.dart';
 
 class AppVersionUpdateListener extends ConsumerStatefulWidget {
   const AppVersionUpdateListener({super.key, required this.child});

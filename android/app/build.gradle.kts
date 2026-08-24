344a9a3c09ebc2dddb6714a8fcf32b539cad8766
LiggMax <wenzhouli06@gmail.com>
2026-08-24 16:12:01 +0800

    chore(android)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -58,12 +58,6 @@ android {
             } else {
                 signingConfigs.getByName("debug")
             }
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
-            )
         }
     }
 }
